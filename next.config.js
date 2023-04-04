bbf7cfa7794a6241aa7235babacc0ad597224bc3
ManpreetSingh-o18 <manpreet.s.git@offer18.com>
2023-04-04 15:34:23 +0530

    YML Added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  webpack(config) {
-    config.experiments = { ...config.experiments, topLevelAwait: true };
-    return config;
-  },
-};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   webpack(config) {
+//     config.experiments = { ...config.experiments, topLevelAwait: true };
+//     return config;
+//   },
+// };
 
-export default nextConfig;
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+}
+// export default nextConfig;
